90012673aec92d92aefe22728b990f67c521a578
edup94 <eduardo.paredes@estudiantes.utec.edu.uy>
2021-05-31 15:00:16 +0000

    agrego archivos vistas

---
+++ src/js/views/login.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
+
+export const Login = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="container">
+			
+		</div>
+	);
+};
---
+++ src/js/views/register.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
+
+export const Register = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="container">
+			
+		</div>
+	);
+};
