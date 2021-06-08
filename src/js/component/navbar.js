e3d820df60983dab0e3582adeec3119a6e0eab15
edup94 <eduardo.paredes@estudiantes.utec.edu.uy>
2021-06-08 14:53:05 +0000

    subo repo actualizado

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -7,32 +7,27 @@ export const Navbar = () => {
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">enBICIando</span>
 			</Link>
-			{/*<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-            </div>*/}
 			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
 				<span className="navbar-toggler-icon" />
 			</button>
 			<div className="collapse navbar-collapse" id="collapsibleNavbar">
 				<ul className="nav nav-pills ml-auto">
-					<li clasNames="nav-item">
+					<li className="nav-item">
 						<a className="btn btn-outline-primary" href="#">
 							Home
 						</a>
 					</li>
-					<li clasNames="nav-item">
+					<li className="nav-item">
 						<a className="btn btn-outline-primary ml-1" href="#">
 							Favoritos
 						</a>
 					</li>
-					<li clasNames="nav-item">
+					<li className="nav-item">
 						<a className="btn btn-outline-primary ml-1" href="#">
 							Bici-friendly
 						</a>
 					</li>
-					<li clasNames="nav-item">
+					<li className="nav-item">
 						<a className="btn btn-outline-primary ml-1" href="#">
 							Mis rutas
 						</a>
