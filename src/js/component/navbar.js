82271852d72d4a108b366863c675428682406844
DamianEmme <ademmenegger@gmail.com>
2021-06-08 13:45:02 +0000

    subo navbar, tarjeta favoritos, perfil de negocio

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,14 +3,41 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-expand-md navbar-light bg-light mb-3 pl-5 pr-5">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">enBICIando</span>
 			</Link>
-			<div className="ml-auto">
+			{/*<div className="ml-auto">
 				<Link to="/demo">
 					<button className="btn btn-primary">Check the Context in action</button>
 				</Link>
+            </div>*/}
+			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+				<span className="navbar-toggler-icon" />
+			</button>
+			<div className="collapse navbar-collapse" id="collapsibleNavbar">
+				<ul className="nav nav-pills ml-auto">
+					<li clasNames="nav-item">
+						<a className="btn btn-outline-primary" href="#">
+							Home
+						</a>
+					</li>
+					<li clasNames="nav-item">
+						<a className="btn btn-outline-primary ml-1" href="#">
+							Favoritos
+						</a>
+					</li>
+					<li clasNames="nav-item">
+						<a className="btn btn-outline-primary ml-1" href="#">
+							Bici-friendly
+						</a>
+					</li>
+					<li clasNames="nav-item">
+						<a className="btn btn-outline-primary ml-1" href="#">
+							Mis rutas
+						</a>
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
