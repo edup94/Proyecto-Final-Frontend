aa001b64b07730635e2fa08f5c425e5a1fb7a123
DamianEmme <ademmenegger@gmail.com>
2021-06-16 04:54:49 +0000

    footer arreglado

--- src/js/component/localCard.js
+++ src/js/component/localCard.js
@@ -28,32 +28,6 @@ export const LocalCard = props => (
 						<p3>{props.direccion}</p3>
 					</div>
 				</div>
-				<div className="comentarios pt-3">
-					<div className="comment">
-						<p>
-							<strong>Claringa</strong>
-							<div>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris iaculis ipsum id
-								consequat convallis. Duis eget nulla malesuada, scelerisque ex sit amet, viverra diam.
-								Nunc sagittis ornare justo eget pretium. Duis id malesuada odio. Pellentesque massa
-								velit, finibus ac lorem a, porttitor fermentum augue. Maecenas vitae eros eu quam
-								faucibus rhoncus vel vitae mi. Maecenas leo ipsum, imperdiet at tortor eu, consectetur
-								suscipit est. Integer lacinia aliquet eros, non rutrum nibh tristique a.
-							</div>
-						</p>
-					</div>
-					<div className="comment">
-						<p>
-							<strong>Edward Walls</strong>
-							<div>
-								Phasellus arcu lorem, efficitur hendrerit diam non, rhoncus maximus ipsum. Etiam eu
-								blandit odio, at volutpat nulla. Nullam ornare viverra tortor, nec vestibulum neque.
-								Nulla facilisi. Donec eleifend nibh purus, eget porttitor lectus vulputate quis. Quisque
-								nec blandit tortor. Donec ac mauris id quam molestie convallis vel non ipsum.
-							</div>
-						</p>
-					</div>
-				</div>
 			</div>
 		</div>
 	</div>
